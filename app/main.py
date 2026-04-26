d9e8eb4e79991667e7f8760b8457511ac6161998
pmdasilva <paulo.estudante9904@gmail.com>
2026-04-26 11:30:06 -0300

    :sparkles: feat: funcionalidade utils - clear_terminal 1.0v adicionada

--- app/main.py
+++ app/main.py
@@ -2,9 +2,34 @@ import crud.read
 import crud.insert
 import crud.update
 import crud.delete
+import utils.clear_terminal
 
-crud.read.show_menu()
-crud.insert.adding_hamburguers()
-crud.insert.list_humburguers()
-crud.update.update_list_hamburguers()
-crud.delete.delete_hamburguers()
+
+def application():
+    option = 0
+    while option != 5:
+        crud.read.show_menu()
+        option = int(input("Digite a opção desejada: "))
+
+        if option == 1:
+            utils.clear_terminal.clear_terminal()
+            crud.insert.adding_hamburguers()
+        elif option == 2:
+            utils.clear_terminal.clear_terminal()
+            crud.insert.list_humburguers()
+
+        elif option == 3:
+            utils.clear_terminal.clear_terminal()
+            crud.update.update_list_hamburguers()
+        elif option == 4:
+            utils.clear_terminal.clear_terminal()
+            crud.delete.delete_hamburguers()
+        elif option == 5:
+            utils.clear_terminal.clear_terminal()
+            print("Saindo do programa...")
+        else:
+            utils.clear_terminal.clear_terminal()
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+
+application()
