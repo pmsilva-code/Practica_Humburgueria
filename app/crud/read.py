ee81e95a51b8def063d4e05e0806a56a102a752b
pmdasilva <pmdasilvaestud@gmail.com>
2026-04-26 01:48:49 -0300

    :sparkles: feat: foi implementado  as funcionabilidades 1.0v - read/insert

---
+++ app/crud/read.py
@@ -0,0 +1,9 @@
+def show_menu():
+    print(f'\n --- MENU HAMBURGARIA ---')
+    print(f'1. ADDING a hambuguer')
+    print(f'2. LIST a hambuguer')
+    print(f'3. UPDATE a hambuguer')
+    print(f'4. DELETE a hambuger')
+    print(f'5. Leave')
+
+    
---
+++ app/main.py
@@ -0,0 +1,7 @@
+import crud.read
+import crud.insert
+
+crud.read.show_menu()
+crud.insert.adding_hamburguers()
+crud.insert.list_humburguers()
+
